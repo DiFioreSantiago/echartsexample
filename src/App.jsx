42476b3ac3408229f264a4ea6bbca9629f1a3e4e
Santiago Di Fiore <difiore.san@gmail.com>
2022-05-10 11:00:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Echarts from './Echarts';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Echarts />
+    </div>
+  )
+}
+
+export default App
